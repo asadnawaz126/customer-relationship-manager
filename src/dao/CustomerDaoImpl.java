d419b9504c8f70fe83f76a49dff667be8dcb9e9f
Asad Nawaz <asadnawazmobilecrazy@gmail.com>
2019-11-18 13:26:00 -0600

    added CustomerDao and CustomerDaoImpl

---
+++ src/dao/CustomerDaoImpl.java
@@ -0,0 +1,32 @@
+package dao;
+
+import entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class CustomerDaoImpl implements CustomerDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        Session session = sessionFactory.getCurrentSession();
+
+        Query<Customer> query = session.createQuery("from Customer", Customer.class);
+
+        List<Customer> customers = query.getResultList();
+
+        return customers;
+    }
+}
