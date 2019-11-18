121fd5a4e07c333751b8df7ac6c0eed98a68883b
Asad Nawaz <asadnawazmobilecrazy@gmail.com>
2019-11-18 13:35:32 -0600

    Added customerDao dependency in CustomerController and implemented listCustomers method

--- src/springdemo/CustomerController.java
+++ src/springdemo/CustomerController.java
@@ -1,16 +1,28 @@
 package springdemo;
 
 
+import dao.CustomerDao;
+import entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @Autowired
+    private CustomerDao customerDao;
+
     @RequestMapping("/list")
     public String listCustomers(Model model){
+
+        List<Customer> customers = customerDao.getCustomers();
+
+        model.addAttribute("customer", customers);
 
         System.out.println("This is being called");
         return "list-customers";
